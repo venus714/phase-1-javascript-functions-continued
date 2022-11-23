6d9c2c9170ded6b1a83c77897b293ccd1850c674
Student <venus.kabera@student.moringaschool.com>
2022-11-23 13:02:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+}
+function mondayWork(words="go to the office"){
+    return(`This Monday, I will ${words}.`)
+
+}
+function wrapAdjective(asteri='*'){
+    return function wrapAdjective(desc="a hard worker"){
+     return(`You are ${asteri}${desc}${asteri}!`)
+}}
